97d2cf373e25273ad85a316e3c73acddd7f411ec
mojtaba-jsx <mojtaba.jsx@gmail.com>
2025-02-03 08:28:50 +0330

    [Features]- Add esponsive And Title

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -4,10 +4,15 @@ import { TbTruckDelivery } from "react-icons/tb";
 import { FaHeadset } from "react-icons/fa";
 import { MdOutlineSecurity } from "react-icons/md";
 import { FaUndoAlt } from "react-icons/fa";
+import { FaRocket } from "react-icons/fa6";
 function Features() {
   return (
     <div className="features">
       <div className="container">
+        <h1 className="features-title">
+          خدمات ما 
+          <FaRocket className="features-title__icon" />
+        </h1>
         <div className="features__wrapper">
           <div className="features__box">
             <TbTruckDelivery className="features-box-icon" />
@@ -15,14 +20,17 @@ function Features() {
               تحویل  در سریع ترین زمان
             </p>
           </div>
+
           <div className="features__box">
             <FaHeadset className="features-box-icon" />
             <p className="features__box-text">پاسخ گویی 24 ساعته پشتیبان </p>
           </div>
+
           <div className="features__box">
             <MdOutlineSecurity className="features-box-icon" />
             <p className="features__box-text">پرداخت امن و سریع</p>
           </div>
+
           <div className="features__box">
             <FaUndoAlt className="features-box-icon" />
             <p className="features__box-text">
