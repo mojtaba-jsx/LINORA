6ec5b629662beed889f048f3df6c38ca1df1e5c6
mojtaba-jsx <mojtaba.jsx@gmail.com>
2025-02-05 08:01:53 +0330

    [Basket]- Add HTML Template

--- src/components/Basket/Basket.jsx
+++ src/components/Basket/Basket.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-import './Basket.css'
+import React from "react";
+import "./Basket.css";
+import { IoCloseOutline } from "react-icons/io5";
 function Basket() {
   return (
-    <div>Basket</div>
-  )
+    <div className="basket">
+      <div className="basket__header">
+        <h1 className="basket__header-title">سبد خرید</h1>
+        <IoCloseOutline className="basket__header-icon" />
+      </div>
+
+      <div className="basket__box">
+        <img src="./src/assets/images/basket/basket.png" alt="" className="basket__box-image" />
+        <span className="basket__box-text">
+          سبد خرید خالی است .
+        </span>
+      </div>
+
+      <div className="basket__checkout">
+        <a href="#" className="basket__checkout-link">
+          ادامه به پرداخت
+          <span className="basket__checkout-link-text">
+            0.00 تومان 
+          </span>
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default Basket
+export default Basket;
