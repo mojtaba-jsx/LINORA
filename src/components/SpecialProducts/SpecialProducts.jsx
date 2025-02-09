fbb96d4f6113ed1e81829f94ea4d63ecca2d8762
mojtaba-jsx <mojtaba.jsx@gmail.com>
2025-02-09 09:19:59 +0330

    [specialProducts]- Add HTML Template And CSS Styles

--- src/components/SpecialProducts/SpecialProducts.jsx
+++ src/components/SpecialProducts/SpecialProducts.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import "./SpecialProducts.css";
+
 function SpecialProducts() {
   const [productsData, setProductsData] = useState([
     {
       id: 1,
       title: "کیف نایک ",
+      caption: "جنس عالی و با کیفیت",
       originalPrice: "400000",
       finalPrice: "320000",
       discountPercent: "20",
@@ -13,6 +15,7 @@ function SpecialProducts() {
     {
       id: 2,
       title: " کلاه پشمی ادیداس ",
+      caption: "جنس عالی و با کیفیت",
       originalPrice: "100000",
       finalPrice: "70000",
       discountPercent: "30",
@@ -21,6 +24,7 @@ function SpecialProducts() {
     {
       id: 3,
       title: "کتونی نایک ",
+      caption: "جنس عالی و با کیفیت",
       originalPrice: "1000000",
       finalPrice: "900000",
       discountPercent: "10",
@@ -29,7 +33,8 @@ function SpecialProducts() {
     {
       id: 4,
       title: "عینک آفتابی ری بن ",
-      originalPrice: "500000 تومان",
+      caption: "جنس عالی و با کیفیت",
+      originalPrice: "500000 ",
       finalPrice: "350000",
       discountPercent: "30",
       image: "./src/assets/images/special-products/4.png",
@@ -37,6 +42,7 @@ function SpecialProducts() {
     {
       id: 5,
       title: "ساعت کلاسیک ",
+      caption: "جنس عالی و با کیفیت",
       originalPrice: "2000000",
       finalPrice: "2000000",
       discountPercent: "0",
@@ -48,11 +54,63 @@ function SpecialProducts() {
       <div className="conatiner">
         <div className="special-products__wrapper">
           <div className="special-products__right">
+            <div className="special-products__box right-box">
+              <img
+                src="./src/assets/images/special-products/1.png"
+                alt=""
+                className="special-products__box-image"
+              />
+              <div className="special-products__box__infos">
+                <div className="special-products__box__infos-right">
+                  <span className="special-products__box__infos-right-title">
+                    کفش نایک
+                  </span>
+                  <span className="special-products__box__infos-right-caption">
+                    کیفیت عالی و با دوام
+                  </span>
+                </div>
 
+                <div className="special-products__box__infos-left">
+                  <span className="special-products__box__infos-left-orgPrice">
+                    120000 تومان
+                  </span>
+                  <span className="special-products__box__infos-left-finalPrice">
+                    100000 تومان
+                  </span>
+                </div>
+              </div>
+            </div>
           </div>
 
           <div className="special-products__left">
-            
+            {productsData.slice(1, 5).map((product) => (
+              <div className="special-products__box" key={product.id}>
+                <img
+                  src={product.image}
+                  alt=""
+                  className="special-products__box-image"
+                />
+                <div className="special-products__box__infos">
+                  <div className="special-products__box__infos-right">
+                    <span className="special-products__box__infos-right-title">
+                      {product.title}
+                    </span>
+                    <span className="special-products__box__infos-right-caption">
+                      {product.caption}
+                    </span>
+                  </div>
+
+                  <div className="special-products__box__infos-left">
+                    <span className="special-products__box__infos-left-orgPrice">
+                      {product.originalPrice} تومان
+                    </span>
+                    <span className="special-products__box__infos-left-finalPrice">
+                      {product.finalPrice} تومان
+                    </span>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
