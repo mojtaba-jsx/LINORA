ab7918b08cc5de8d4cf4de00a29697bf8a71a5e9
mojtaba-jsx <mojtaba.jsx@gmail.com>
2025-02-10 11:07:29 +0330

    [SellingProductItem,SellingProducts]- Add CSS Style

--- src/components/SellingProductItem/SellingProductItem.jsx
+++ src/components/SellingProductItem/SellingProductItem.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./SellingProductItem.css";
-function SellingProductItem() {
+function SellingProductItem({ productInfo }) {
   return (
     <div className="selling-product-item">
       <div className="selling-product-item__right">
         <img
-          src="./src/assets/images/SellingProductsImages/watch.png"
+          src={productInfo.image}
           alt=""
           className="selling-product-item__right-image"
         />
@@ -13,17 +13,17 @@ function SellingProductItem() {
 
       <div className="selling-product-item__left">
         <span className="selling-product-item__left-title">
-          ساعت استیل RedQ
+          {productInfo.title}
         </span>
         <span className="selling-product-item__left-caption">
-          ساعت های با کیفت و ...
+          {productInfo.caption}
         </span>
         <span className="selling-product-item__left__prices">
           <span className="selling-product-item__left__prices-final">
-            2000000 تومان
+            {productInfo.finalPrice} تومان
           </span>
           <span className="selling-product-item__left__prices-original">
-            1400000 تومان
+            {productInfo.originalPrice} تومان
           </span>
         </span>
       </div>
--- src/components/SellingProducts/SellingProducts.jsx
+++ src/components/SellingProducts/SellingProducts.jsx
@@ -1,21 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./SellingProducts.css";
-import MainTitle from '../MainTitle/MainTitle'
+import MainTitle from "../MainTitle/MainTitle";
+import SellingProductItem from "../SellingProductItem/SellingProductItem";
 function SellingProducts() {
+  const [sellingProductsInfos, setSellingProductsInfos] = useState([
+    {
+      id: 1,
+      title: "ساعت استیل RedQ",
+      caption: "ساعت های با کیفت و .",
+      orginalPrice: "2000000 ",
+      finalPrice: "1400000",
+      image: "./src/assets/images/SellingProductsImages/watch.png",
+    },
+    {
+      id: 2,
+      title: "ساعت استیل RedQ",
+      caption: "ساعت های با کیفت و .",
+      orginalPrice: "2000000 ",
+      finalPrice: "1400000",
+      image: "./src/assets/images/SellingProductsImages/watch.png",
+    },
+    {
+      id: 3,
+      title: "ساعت استیل RedQ",
+      caption: "ساعت های با کیفت و .",
+      orginalPrice: "2000000 ",
+      finalPrice: "1400000",
+      image: "./src/assets/images/SellingProductsImages/watch.png",
+    },
+    {
+      id: 4,
+      title: "ساعت استیل RedQ",
+      caption: "ساعت های با کیفت و .",
+      orginalPrice: "2000000 ",
+      finalPrice: "1400000",
+      image: "./src/assets/images/SellingProductsImages/watch.png",
+    },
+    {
+      id: 5,
+      title: "ساعت استیل RedQ",
+      caption: "ساعت های با کیفت و .",
+      orginalPrice: "2000000 ",
+      finalPrice: "1400000",
+      image: "./src/assets/images/SellingProductsImages/watch.png",
+    },
+    {
+      id: 6,
+      title: "ساعت استیل RedQ",
+      caption: "ساعت های با کیفت و .",
+      orginalPrice: "2000000 ",
+      finalPrice: "1400000",
+      image: "./src/assets/images/SellingProductsImages/watch.png",
+    },
+    {
+      id: 7,
+      title: "ساعت استیل RedQ",
+      caption: "ساعت های با کیفت و .",
+      orginalPrice: "2000000 ",
+      finalPrice: "1400000",
+      image: "./src/assets/images/SellingProductsImages/watch.png",
+    },
+    {
+      id: 8,
+      title: "ساعت استیل RedQ",
+      caption: "ساعت های با کیفت و .",
+      orginalPrice: "2000000 ",
+      finalPrice: "1400000",
+      image: "./src/assets/images/SellingProductsImages/watch.png",
+    },
+    {
+      id:9,
+      title: "ساعت استیل RedQ",
+      caption: "ساعت های با کیفت و .",
+      orginalPrice: "2000000 ",
+      finalPrice: "1400000",
+      image: "./src/assets/images/SellingProductsImages/watch.png",
+    },
+  ]);
   return (
     <div className="selling-products">
       <div className="container">
         <div className="selling-products__header">
-            <MainTitle title={'محصولات در حال فروش'}/>
-           <Link className="selling-products__header-link" to={'/'}>
-                مشاهده همه محصولات
-            </Link>
+          <div className="selling-products__header-title">
+            <MainTitle title={"محصولات در حال فروش"} />
+          </div>
+          <Link className="selling-products__header-link" to={"/"}>
+            مشاهده همه محصولات
+          </Link>
         </div>
 
         <div className="selling-products__wrapper">
-                <div className=""></div>
+          {
+            sellingProductsInfos.map(product=>(
 
+              <SellingProductItem key={product.id} productInfo ={product} />
+            ))
+          }
         </div>
       </div>
     </div>
