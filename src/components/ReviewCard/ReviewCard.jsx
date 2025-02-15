5b1e6c405f8863296207929b2a5d8e4034d41aac
mojtaba-jsx <mojtaba.jsx@gmail.com>
2025-02-15 10:10:38 +0330

    [ReviewSlider-ReviewCard]- Add Swiper JS And Add CSS Styles

--- src/components/ReviewCard/ReviewCard.jsx
+++ src/components/ReviewCard/ReviewCard.jsx
@@ -3,29 +3,25 @@ import "./ReviewCard.css";
 import { FaRegStar } from "react-icons/fa";
 import { FaStar } from "react-icons/fa";
 import { FaStarHalfStroke } from "react-icons/fa6";
-function ReviewCard() {
+function ReviewCard({ username, userImage, body, star }) {
   return (
     <div className="review-card">
       <div className="review-card__top">
         <div className="review-card__top-stars">
-        <FaRegStar />
-        <FaStar />
-        <FaStarHalfStroke />
+          <FaRegStar className="review-card__top-stars-icon" />
+          <FaStarHalfStroke className="review-card__top-stars-icon" />
+          <FaStar className="review-card__top-stars-icon" />
+          <FaStar className="review-card__top-stars-icon" />
+          <FaStar className="review-card__top-stars-icon" />
         </div>
         <div className="review-card__top-user">
-          <img
-            src="./src/assets/images/review-slider/user.png"
-            alt=""
-            className="review-card__top-user-image"
-          />
-          <span className="review-card__top-user-tile">علی کریمی</span>
+          <img src={userImage} alt="" className="review-card__top-user-image" />
+          <span className="review-card__top-user-title">{username}</span>
         </div>
       </div>
 
       <div className="review-card__bottom">
-        <p className="review-card__bottom-text">
-          محصولات خوب و با کیفیتی دارید و تحویل به موقع و سریع هست ممنون
-        </p>
+        <p className="review-card__bottom-text">{body}</p>
       </div>
     </div>
   );
--- src/components/ReviewSlider/ReviewSlider.jsx
+++ src/components/ReviewSlider/ReviewSlider.jsx
@@ -1,12 +1,96 @@
-import React from 'react'
-import './ReviewSlider.css'
-import ReviewCard from '../ReviewCard/ReviewCard'
+import React, { useState } from "react";
+import "./ReviewSlider.css";
+import ReviewCard from "../ReviewCard/ReviewCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import MainTitle from '../MainTitle/MainTitle'
 function ReviewSlider() {
+  const [usersCommentsInfo, setUsersCommentsInfo] = useState([
+    {
+      id: 1,
+      username: "علی کریمی",
+      userImage: "./src/assets/images/review-slider/user.png",
+      body: "محصولات خوب و با کیفیتی دارید و تحویل به موقع و سریع هست ممنون",
+      star:4
+    },
+    {
+      id: 2,
+      username: "علی کریمی",
+      userImage: "./src/assets/images/review-slider/user.png",
+      body: "محصولات خوب و با کیفیتی دارید و تحویل به موقع و سریع هست ممنون",
+      star:4
+    },
+    {
+      id: 3,
+      username: "علی کریمی",
+      userImage: "./src/assets/images/review-slider/user.png",
+      body: "محصولات خوب و با کیفیتی دارید و تحویل به موقع و سریع هست ممنون",
+      star:4
+    },
+    {
+      id: 4,
+      username: "علی کریمی",
+      userImage: "./src/assets/images/review-slider/user.png",
+      body: "محصولات خوب و با کیفیتی دارید و تحویل به موقع و سریع هست ممنون",
+      star:4
+    },
+    {
+      id: 5,
+      username: "علی کریمی",
+      userImage: "./src/assets/images/review-slider/user.png",
+      body: "محصولات خوب و با کیفیتی دارید و تحویل به موقع و سریع هست ممنون",
+      star:4
+    },
+  ]);
   return (
-    <div className='review-slider'>
-        <ReviewCard/>
+    <div className="review-slider">
+      <div className="container">
+        <MainTitle title={'نظرات'}/>
+        <div className="review-slider__wrapper">
+          <Swiper
+            className="review-swiper"
+            modules={[Navigation, Pagination, Autoplay]}
+            loop={true}
+            pagination={{ clickable: true }}
+            autoplay={{
+              delay: 1500,
+              disableOnInteraction: false,
+              pauseOnMouseEnter: true,
+            }}
+            breakpoints={{
+              576: {
+                slidesPerView: 2,
+                spaceBetween: 120,
+              },
+
+              768: {
+                slidesPerView: 2,
+                spaceBetween: 140,
+              },
+
+              1024: {
+                slidesPerView: 3,
+                spaceBetween: 180,
+              },
+              1200: {
+                slidesPerView: 4,
+                spaceBetween: 180,
+              },
+            }}
+          >
+            {usersCommentsInfo.map((comment) => (
+              <SwiperSlide key={comment.id}>
+                <ReviewCard {...comment} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ReviewSlider
+export default ReviewSlider;
