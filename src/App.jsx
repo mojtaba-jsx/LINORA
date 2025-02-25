f3dfa21d2e74de27bbc77c407949274082d2dfab
mojtaba-jsx <mojtaba.jsx@gmail.com>
2025-02-25 09:23:48 +0330

    [Loader]- Add Loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import React, { useState } from "react";
-import { useRoutes } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useRoutes, useLocation } from "react-router-dom";
 import routes from "./routes";
-
+import Loader from "./components/Loader/Loader";
+import { useLoader, LoaderProvider } from "./Contexts/LoaderContext";
 import appContext from "./Contexts/AppContext";
-function App() {
+
+function AppContent() {
   let route = useRoutes(routes);
+  const location = useLocation();
+  const { loading, setLoading } = useLoader();
+
+  // State برای کنترل نمایش صفحه پس از لود شدن
+  const [isPageLoaded, setIsPageLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => {
+      setLoading(false);
+      setIsPageLoaded(true);  // پس از پایان لود، صفحه را نمایش می‌دهیم
+    }, 1200); // زمان لود کردن (۲ ثانیه)
+
+    return () => clearTimeout(timer);
+  }, [location.pathname]);
 
   const [isShowSearchModal, setIsShowSearchModal] = useState(false);
   const [isShowLoginModal, setIsShowLoginModal] = useState(false);
@@ -24,8 +41,17 @@ function App() {
         setIsShowBasketModal,
       }}
     >
-      <div>{route}</div>
+      {loading && <Loader />} {/* نمایش Loader */}
+      {isPageLoaded && <div>{route}</div>} {/* نمایش صفحه بعد از بارگذاری */}
     </appContext.Provider>
+  );
+}
+
+function App() {
+  return (
+    <LoaderProvider>
+      <AppContent />
+    </LoaderProvider>
   );
 }
 
